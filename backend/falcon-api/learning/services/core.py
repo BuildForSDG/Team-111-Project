deb4d38cdc9ff9bd4516157b83a98a481b6b052f
jayson <jaysinscars@gmail.com>
2020-05-31 22:21:19 +0100

    COmmitted

--- backend/falcon-api/learning/services/core.py
+++
@@ -1,9 +0,0 @@
-from learning.services.base import *
-from learning.models import *
-
-
-ApplicationService = ServiceFactory.create_service(Application)
-ApplicationGroupService = ServiceFactory.create_service(ApplicationGroup)
-AccountTypeService = ServiceFactory.create_service(AccountType)
-CategoryService = ServiceFactory.create_service(Category)
-CourseService = ServiceFactory.create_service(Course)
---
+++ setups/application_groups.py
@@ -0,0 +1,25 @@
+obj = dict(
+    klass="ApplicationGroupService",
+    data=[
+        {
+            "name": "User",
+            "code": "user",
+            "applications": []
+        },
+        {
+            "name": "Admin",
+            "code": "admin",
+            "applications": ["admin"]
+        },
+        {
+            "name": "Teacher",
+            "code": "teacher",
+            "applications": ["teacher"]
+        },
+        {
+            "name": "Student",
+            "code": "student",
+            "applications": ["student"]
+        },
+    ]
+)
